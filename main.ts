8d01af069178cbca9614e0955f440417fab54467
jimmyfaat <jimmyfaat@gmail.com>
2020-02-11 23:05:50 +0800

    update

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+namespace Mbit_IR {
+
+    /**
+     * button pushed.
+     */
+    //% blockId=ir_received_left_event
+    //% block="on |%btn| button pressed" shim=Mbit_IR::onPressEvent
+    export function onPressEvent(btn: RemoteButton, body: () => void): void;
+
+    /**
+     * initialises local variablesssss
+     */
+    //% blockId=ir_init
+    //% block="connect ir receiver to %pin" shim=Mbit_IR::init
+    function init(pin: Pins): void;
+}
