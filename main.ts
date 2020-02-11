9ba29f8751b4c902601b66c8eb8ec7791a257112
jimmyfaat <jimmyfaat@gmail.com>
2020-02-12 00:17:36 +0800

    update

--- main.ts
+++ main.ts
@@ -1,18 +1,7 @@
-namespace Mbit_IR {
-
-    /**
-     * button pushed.
-     */
-    //% blockId=ir_received_left_event
-    //% block="on |%btn| button pressed" shim=Mbit_IR::onPressEvent
-    export function onPressEvent(btn: RemoteButton, body: () => void): void {
-    }
-
-    /**
-     * initialises local variablesssss
-     */
-    //% blockId=ir_init
-    //% block="connect ir receiver to %pin" shim=Mbit_IR::init
-    function init(pin: Pins): void {
-    }
-}
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `);
--- shims.d.ts
+++ shims.d.ts
@@ -11,7 +11,7 @@ declare namespace Mbit_IR {
      */
     //% blockId=ir_received_left_event
     //% block="on |%btn| button pressed" shim=Mbit_IR::onPressEvent
-    function onPressEvent(btn: RemoteButton, body: () => void): void;
+    export function onPressEvent(btn: RemoteButton, body: () => void): void;
 
     /**
      * initialises local variablesssss
@@ -20,5 +20,4 @@ declare namespace Mbit_IR {
     //% block="connect ir receiver to %pin" shim=Mbit_IR::init
     function init(pin: Pins): void;
 }
-
 // Auto-generated. Do not edit. Really.
